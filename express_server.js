2aa9b08a80de773b5335986d4ec35945ed605f09
Jake <jakefantin@gmail.com>
2019-09-30 23:25:52 +0000

    added some extra paths/no templates yet

--- express_server.js
+++ express_server.js
@@ -15,3 +15,11 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get('/home', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
