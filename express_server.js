5f742fc0ce5b6ab9cfed1fea30833dd55360ee2b
Jake <jakefantin@gmail.com>
2019-10-01 01:52:56 +0000

    added new form that sends a POST and we catch that post and return ok. Also created a randomString generator for future use

--- express_server.js
+++ express_server.js
@@ -4,10 +4,23 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http:/www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let randomString = '';
+  let alphaNum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklmnopqrstuvwxyz1234567890';
+  for(let i = 0;i<6;i++){
+    randomString += alphaNum[Math.floor(Math.round()*alphaNum.length)];
+  }
+  return randomString;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,6 +29,11 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
 });
 
 app.get("/urls.json", (req, res) => {
@@ -30,6 +48,10 @@ app.get('/home', (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
